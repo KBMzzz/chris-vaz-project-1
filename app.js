154d167a3052549b093ce6c85c6d9265478f4daf
Chris Vaz <chrisvaz17@gmail.com>
2023-10-16 06:32:08 -0400

    added a missing item back to the products section, changed from the @import to @use, reformatted some files.

---
+++ app.js
@@ -0,0 +1,20 @@
+// // SELECT ELEMENTS
+// const productsEl = document.querySelector(".products");
+
+// // RENDER PRODUCTS
+// function renderProducts() {
+//   products.forEach((product) => {
+//     productsEl.innerHTML += `
+//       <div class="pic-1-text">
+//             <img src="${product.imgSrc}" alt="image of a tomato" />
+//             <div class="border">
+//               <p class="tag">${product.name}</p>
+//               <p>lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+//               <p class="price">$370 <s>$350</s></p>
+//               <a href="#" class="button">add to cart</a>
+//             </div>
+//           </div>
+//         `;
+//   });
+// }
+// renderProducts();
